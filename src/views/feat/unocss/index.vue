475dfab7b9d4f17e828ab2e956575522a0d17c86
qianphong <qianphong@163.com>
2022-05-13 18:25:17 +0800

    chore: update

--- src/views/feat/unocss/index.vue
+++ src/views/feat/unocss/index.vue
@@ -1,4 +1,13 @@
 <script lang="ts" setup></script>
+
 <template>
-  <div>Unocss</div>
+  <div>
+    Unocss
+    <div>
+      <h3>ICON</h3>
+      <div>
+        <div class="i-custom-link text-8xl" />
+      </div>
+    </div>
+  </div>
 </template>
--- unocss.config.ts
+++ unocss.config.ts
@@ -1,5 +1,16 @@
 import { defineConfig, presetUno, presetAttributify, presetIcons } from 'unocss'
+import { FileSystemIconLoader } from '@iconify/utils/lib/loader/node-loaders'
 
 export default defineConfig({
-  presets: [presetUno(), presetAttributify(), presetIcons()],
+  presets: [
+    presetUno(),
+    presetAttributify(),
+    presetIcons({
+      collections: {
+        custom: FileSystemIconLoader('./src/assets/icons', (svg) =>
+          svg.replace(/#fff/, 'currentColor')
+        ),
+      },
+    }),
+  ],
 })
